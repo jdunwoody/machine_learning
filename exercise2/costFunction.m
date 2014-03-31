5ff7264a7363e137f33311c53df5ec1efc0eda55
James Dunwoody <jamesd@hooroo.com>
2014-03-31 22:37:41 +1100

    Cost and gradient

--- exercise2/costFunction.m
+++ exercise2/costFunction.m
@@ -7,9 +7,9 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% You need to return the following variables correctly
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -20,13 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X * theta);
 
+J = (1/m) * sum(-y .* log(h) .- (1 .- y) .* log(1 .- h));
 
-
-
-
-
-
+grad = (1/m) .* X' * (h - y);
 % =============================================================
 
 end
